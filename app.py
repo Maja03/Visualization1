21ec0c46e8f48da85f4e9923ad5dc800bce9e967
Maja Czekała <112630417+Maja03@users.noreply.github.com>
2024-05-17 03:31:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
+from flask import Flask, send_file, jsonify
 import pandas as pd
-from flask import Flask, render_template, jsonify
-
-
-mental_health_data = pd.read_csv('Student Mental health.csv')
-mental_health_json = mental_health_data.to_json(orient='records')
 
 app = Flask(__name__)
 
+# Load mental health data from CSV
+mental_health_data = pd.read_csv('Student Mental health.csv')
+mental_health_json = mental_health_data.to_json(orient='records')
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return send_file('index.html')
 
 @app.route('/data', methods=['GET'])
 def get_data():
